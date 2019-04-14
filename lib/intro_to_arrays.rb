be32367b1b5aeb0659db4eaee87b7e88b389bd8f
Nicole Dow <nikki.dow71@gmail.com>
2019-04-14 19:47:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,9 @@ def instantiate_new_array
   my_new_array = []
   return my_new_array
 end
+
+def array_with_two_elements
+  my_two_array = Array.new
+  my_two_array << "hello"
+  my_two_array << "world"
+end
